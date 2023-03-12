200decf9b23eb8ae46739ee24f7ddbeeb9886679
Muhammad Hidayatulloh S <m.hidayatulloh1@gmail.com>
2023-03-12 13:52:29 +0700

    users list

--- config/api_router.py
+++ config/api_router.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
+from chronicle_test.users.api.views import UserViewSet
+
 if settings.DEBUG:
     router = DefaultRouter()
 else:
     router = SimpleRouter()
 
-# router.register("users", UserViewSet)
+router.register("users", UserViewSet)
 
 
 app_name = "api"
